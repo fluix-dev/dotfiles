6d85d17a0d1dd7fafa9679bbb6c9477561173e6d
TheAvidDev <AvidDev@protonmail.com>
2020-07-04 21:23:00 -0400

    Move screenshotting into a script

---
+++ scripts/screenshot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#
+# Script that takes a screenshot of:
+#  "window" - The active window, including borders
+#  "select" - A selection the user makes
+# Placing it into the ~/Screenshots/ directory and into the clipboard
+
+TYPE=$1
+
+mkdir -p ~/Screenshots
+if [ $1 == "window" ]; then
+  SNIP_DIMENSIONS=$(swaymsg -t get_tree | jq -r '.. | select(.focused?) | .rect | "\(.x),\(.y) \(.width)x\(.height)"')
+elif [ $1 == "select" ]; then
+  SNIP_DIMENSIONS=$(slurp)
+fi
+
+if [ $SNIP_DIMENSIONS != "selection cancelled" ]; then
+  grim -g "$SNIP_DIMENSIONS" - | wl-copy -t image/png
+  wl-paste > ~/Screenshots/screenshot_$(date +'%Y%m%d_%H%M%S.png')
+fi
