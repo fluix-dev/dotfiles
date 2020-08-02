545c12337343ffcb8aedf3cdaaacc640c79728bc
TheAvidDev <AvidDev@protonmail.com>
2020-08-01 22:49:30 -0400

    Have LED strip script read output

--- scripts/system-sleep-led-strip.sh
+++ scripts/system-sleep-led-strip.sh
@@ -6,5 +6,6 @@
 if [ "${1}" == "pre" ]; then
   echo -n "[0, 0, 0]" > /dev/ttyACM0
 elif [ "${1}" == "post" ]; then
-  echo -n "[255, 150, 175]" > /dev/ttyACM0
+  echo -n "[30, 255, 0]" > /dev/ttyACM0
 fi
+cat /dev/ttyACM0 > /dev/null
