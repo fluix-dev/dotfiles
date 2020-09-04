0ee02a6fab717c91e145d98880788f13f7965445
TheAvidDev <AvidDev@protonmail.com>
2020-09-04 13:39:44 -0400

    Change led color and clarify color order

--- scripts/system-sleep-led-strip.sh
+++ scripts/system-sleep-led-strip.sh
@@ -2,10 +2,11 @@
 
 #
 # Script that runs on suspend and resume which changes the colour of LED strip.
+# The colours are set with [r, b, g]
 #
 if [ "${1}" == "pre" ]; then
   echo -n "[0, 0, 0]" > /dev/ttyACM0
 elif [ "${1}" == "post" ]; then
-  echo -n "[30, 255, 0]" > /dev/ttyACM0
+  echo -n "[255, 0, 64]" > /dev/ttyACM0
 fi
 cat /dev/ttyACM0 > /dev/null
